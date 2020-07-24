ace41c69723b2d3095a0de0ee8507e88e2dbb00a
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-24 16:38:58 +0000

    Part2_LizvethM

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-
 #for loops
 #outer loop
-for i in range(6):
+for i in range(10):
   print("Outer loop | i = " + str(i))
 #inner loop
-for j in range(6):
-  print("inner loop | i = "+ str(i)+ " | j = "+ str(j))
-  
+  for j in range(10):
+    print("inner loop | i = "+ str(i)+ " | j = "+ str(j))
+#nums are even
+    if(i%2)==0 and (j%2)==0:
+      print("even")
+
+
